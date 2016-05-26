b515265ac8f615d2f21c9cdff18f12f929c68b12
firejune <firejune@gmail.com>
2016-05-26 17:56:40 +0900

    Using native require of injection script

--- scripts/webpack.backend.js
+++ scripts/webpack.backend.js
@@ -9,17 +9,46 @@
  */
 'use strict';
 
+const fs = require('fs');
 const webpack = require('webpack');
 const __DEV__ = process.env.NODE_ENV !== 'production';
 
+const nodeModules = {};
+fs.readdirSync('node_modules').forEach(module => {
+  if (module !== '.bin') {
+    nodeModules[module] = true;
+  }
+});
+
+const nodeModulesTransform = function(context, request, callback) {
+  // search for a '/' indicating a nested module
+  const slashIndex = request.indexOf('/');
+  let rootModuleName;
+  if (slashIndex === -1) {
+    rootModuleName = request;
+  } else {
+    rootModuleName = request.substr(0, slashIndex);
+  }
+
+  // Match for root modules that are in our node_modules
+  if (nodeModules.hasOwnProperty(rootModuleName)) {
+    callback(null, `commonjs ${request}`);
+  } else {
+    callback();
+  }
+};
+
+nodeModules.electron = 'commonjs electron';
+
 module.exports = {
   devtool: __DEV__ ? 'cheap-module-eval-source-map' : false,
+
+  target: 'node',
+
   entry: {
     inject: './src/GlobalHook.js',
     backend: './src/backend.js',
   },
-
-  target: 'node',
 
   output: {
     path: './build',
@@ -34,17 +63,19 @@ module.exports = {
       query: {
         babelrc: false,
         presets: [
+          'es2015',
           'react',
           'stage-0'
         ],
         plugins: [
-          'transform-es2015-modules-commonjs',
-          'transform-es2015-function-name',
-          'transform-remove-console'
+          'transform-remove-console',
+          'transform-runtime'
         ]
       }
     }],
   },
+
+  externals: nodeModulesTransform,
 
   plugins: [
     new webpack.DefinePlugin({
