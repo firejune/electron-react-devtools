4b59c11e835e1e27dec435400dd96130644f03cc
firejune <firejune@gmail.com>
2017-11-15 14:42:32 +0900

    Added error handling

--- api.js
+++ api.js
@@ -3,15 +3,23 @@ const electron = require('electron')
 // devtools-extension install
 exports.install = () => {
   if (!electron.remote) {
-    throw new Error('React DevTools cannot be installed from within the main process.')
+    throw new Error('React DevTools can only be installed from an renderer process.')
   }
 
+  const extensions = electron.remote.BrowserWindow.getDevToolsExtensions()
+  if (extensions && extensions['React Developer Tools']) return
+
+
+  const path = electron.remote.BrowserWindow.addDevToolsExtension(__dirname)
   console.log(`Installing React DevTools from ${__dirname}`)
 
-  return electron.remote.BrowserWindow.addDevToolsExtension(__dirname)
+  return path
 }
 
 // devtools-extension uninstall
 exports.uninstall = () => {
+  if (!electron.remote) {
+    throw new Error('React DevTools can only be uninstalled from an renderer process.')
+  }
   return electron.remote.BrowserWindow.removeDevToolsExtension('React Developer Tools')
 }
